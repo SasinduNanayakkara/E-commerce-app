97ed4ec530636c535b359e4fad61e23ac19259cb
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-19 02:44:21 +0530

    add react dom

--- src/App.js
+++ src/App.js
@@ -7,13 +7,36 @@ import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
 import PaymentForm from "./components/PaymentForm";
+import stripeContainer from "./components/stripeContainer";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Routes,
+  Navigate,
+  Switch
+} from "react-router-dom";
+import Products from "./components/Products";
+
 
 const App =() => {
+  const user = true;
   return(
-    <div>
-      <PaymentForm/>
+    <Router>
+      <Routes>
+      {/* latest react dom version */}
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/products/:category" element={<ProductList/>} />
+        <Route path="/product/:id" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>} />
+        <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>} />
+
+
+
+      </Routes>
       
-    </div>
+    </Router>
   ) 
 };
 
